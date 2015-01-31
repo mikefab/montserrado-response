7e90f1eb042269c6d6b59c3abe20eb6684d0f5b4
mikefab <mikefabrikant@gmail.com>
2015-01-31 20:06:11 +0000

    Run zone seed rake just once

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -3,7 +3,6 @@ class Zone < ActiveRecord::Base
   has_and_belongs_to_many :teams, :join_table => "teams_zones"
   
   def self.seed_data
-    Partners2::Application.load_tasks
     Rake::Task['csv:import_epi_zones'].invoke
   end
 end
