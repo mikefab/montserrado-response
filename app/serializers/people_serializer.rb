ec59063857011d203b400603aecc3a0a478b8545
mikefab <mikefabrikant@gmail.com>
2015-02-01 16:43:19 +0000

    full name in people serializer

--- app/serializers/people_serializer.rb
+++ app/serializers/people_serializer.rb
@@ -1,3 +1,3 @@
 class PeopleSerializer < ActiveModel::Serializer
-  attributes :first_name, :middle_name, :last_name, :phone, :email, :is_supervisor
+  attributes :full_name, :phone, :email, :is_supervisor
 end
