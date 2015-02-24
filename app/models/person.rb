2eb1fe236ac9ce8cb7bd7a760cbb68dea2b4ff7a
mikefab <mikefabrikant@gmail.com>
2015-02-24 11:24:11 +0000

    Remove validation of email. Some people don't have one

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,7 @@
 class Person < ActiveRecord::Base
   has_and_belongs_to_many   :teams, :join_table => "people_teams"
   has_many :orgs, :through => :teams
-  validates_email_format_of :email, :message => 'is not looking good'
+  #validates_email_format_of :email, :message => 'is not looking good'
   validates_presence_of :teams
 
   def full_name
