a875384a1c8e0259cc6a02bf99e779108444e5a4
mikefab <mikefabrikant@gmail.com>
2015-03-09 11:51:14 +0000

    Person can belong to an org now.

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,7 +5,7 @@ class Person < ActiveRecord::Base
   #has_many :orgs, :through => :teams
 
   #validates_email_format_of :email, :message => 'is not looking good'
-  validates_presence_of :teams
+  #validates_presence_of :teams
 
   def full_name
   	"#{self.first_name} #{self.middle_name} #{self.last_name}"
