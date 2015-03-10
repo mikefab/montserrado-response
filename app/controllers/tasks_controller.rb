d53bc1db8e6ecfc9c70932e8f8859f2921e265a3
mikefab <mikefabrikant@gmail.com>
2015-03-10 12:29:21 +0000

    Task order by name

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.all
+    @tasks = Task.all.sort_by{|t| t.name}
   end
 
   # GET /tasks/1
