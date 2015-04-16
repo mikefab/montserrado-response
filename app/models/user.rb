4b522b7cb2577d2b08d6272670aad9a2bce86579
mikefab <mikefabrikant@gmail.com>
2015-04-16 15:08:28 +0000

    Remove 'editor' from role selection

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  ROLES = %i[none admin editor viewer]
-
+  #ROLES = %i[none admin editor viewer]
+  ROLES = %i[none admin viewer]
 end
